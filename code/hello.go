0e9d1287275079da6bf0e786b7ecf976a112be16
Dharmit <dharmitshah009@gmail.com>
2022-01-24 19:08:33 +0530

    tour of go: switches

---
+++ code/hello.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	println("This is the dirty println()")
+	fmt.Println("vim-go")
+}
---
+++ code/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+	for x := 0; x < 10; {
+		fmt.Println("Priyanka tu Python padh !!!")
+		x += 1
+	}
+}
